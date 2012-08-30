744d0c3d94bb27b88992ddee8282558211c79c81
Matthew Hielscher <mhielscher@gmail.com>
2012-08-29 23:23:52 -0700

    Fixed retweet interactive mode.

--- retweet.py
+++ retweet.py
@@ -14,23 +14,27 @@ import oauth2 as oauth
 import time
 import wcommon
 
-status = None
+tweetid = None
 
 if len(sys.argv) < 2 or sys.argv[1] == "-i": #interactive mode
-	while not status:
-		tweetid = long(raw_input("Tweet ID to RT: "))
+    while not tweetid:
+        try:
+            tweetid = long(raw_input("Tweet ID to RT: "))
+        except ValueError:
+            continue
 elif len(sys.argv) > 1:
-	tweetid = long(sys.argv[1])
+    tweetid = long(sys.argv[1])
 else:
-	sys.exit(1)
-		
+    sys.exit(1)
+        
 
 url = "http://api.twitter.com/1/statuses/retweet/%d.xml" % (tweetid)
 data = []
 postdata = urllib.urlencode(data)
 r, c = wcommon.oauth_req(url, http_method="POST", post_body=postdata)
 if r.status != 200:
-	print "Retweeting did not succeed: Status %d" % (r.status)
-	print c
+    print "Retweeting did not succeed: Status %d" % (r.status)
+    print c
+    sys.exit(r.status)
 
 
