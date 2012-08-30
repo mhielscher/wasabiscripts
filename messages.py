7abcb950793c5d186d722be096170d2453e55717
Matthew Hielscher <mhielscher@gmail.com>
2012-08-29 23:43:05 -0700

    Made messages.py download more than 10 DMs, plus sent DMs.

--- messages.py
+++ messages.py
@@ -22,10 +22,8 @@ def writelog(body):
 	log = codecs.getwriter('utf8')(open('messages.log', 'a'))
 	if len(data) == 200:
 		print >>log, "-- May have lost some tweets here [%s]" % (time.strftime("%a %b %d %H:%M:%S +0000 %Y"))
-	for i in xrange(len(data)-1, -1, -1):
-		tweet = data[i]
-		link = "http://twitter.com/"+tweet['user']['screen_name']+"/status/"+str(tweet['id'])
-		print >>log, "<%s> %s [%s] <%s>" % (tweet['user']['screen_name'], tweet['text'], tweet['created_at'], link)
+	for tweet in data:
+		print >>log, "<@%s to @%s> %s [%s]" % (tweet['sender_screen_name'], tweet['recipient_screen_name'], tweet['text'], tweet['created_at'])
 	log.close()
 	idfile = open('messages.id', 'w')
 	print >>idfile, data[0]['id']
@@ -59,9 +57,9 @@ received = 1
 alldata = []
 page = 1
 while page < 2 or (received > 20 and page < 20):
-	url = 'http://api.twitter.com/1/direct_messages.xml'
+	url = 'http://api.twitter.com/1/direct_messages.json'
 	head = ['Expect: ']
-	data = [('count', 10)]
+	data = [('count', 20), ('page', page)]
 	#data = [('count', 200)]
 	postdata = urllib.urlencode(data)
 	url = url+'?'+postdata
@@ -77,6 +75,29 @@ while page < 2 or (received > 20 and page < 20):
 	received = len(jdata)
 	print str(received)+" messages"
 	page += 1
+
+page = 1
+while page < 2 or (received > 20 and page < 20):
+	url = 'http://api.twitter.com/1/direct_messages/sent.json'
+	head = ['Expect: ']
+	data = [('count', 200), ('page', page)]
+	#data = [('count', 200)]
+	postdata = urllib.urlencode(data)
+	url = url+'?'+postdata
+	print url
+	r, c = wcommon.oauth_req(url)
+	if r.status > 200:
+		print "Request failed: %d" % (r.status)
+		sys.exit(1)
+	#print c
+	jdata = json.loads(c)
+	#print jdata
+	alldata = alldata + jdata
+	received = len(jdata)
+	print str(received)+" messages"
+	page += 1
+
+alldata.sort(key=lambda t: t['id'])
 	
 writelog(alldata)
 
